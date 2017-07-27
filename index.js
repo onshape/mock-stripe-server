946fabbc7625281fd795b806038f0a8049c3f3a2
mbarr <mbarr@onshape.com>
2017-07-27 15:51:29 -0400

    Better data modeling, proper error handling, subscriptions and invoices. Eslint updates / fixes. Config file parsing to bootstrap organizations.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 'use strict';
 
+const fs = require('fs');
+
+let config;
+
+if (process.argv[2]) {
+  console.log(`Loading config file ${ process.argv[2] }...`);
+  try {
+    config = JSON.parse(fs.readFileSync(process.argv[2]));
+  } catch (error) {
+    console.log('Failed to load config file: ', error);
+  }
+}
+
 const StripeServer = require('./lib/stripeServer');
-const stripeServer = new StripeServer();
+const stripeServer = new StripeServer(config);
 
 stripeServer.boot();
