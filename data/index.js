0242d854cfd743662fdfbba648ac8673afef61d8
mbarr <mbarr@onshape.com>
2017-07-31 14:37:28 -0400

    More handling for charges and invoices, better modelling and error handling.

--- data/index.js
+++ data/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  cards: require('./cards.json')
+  cards: require('./cards.json'),
+  plans: {
+    intervals: {
+      day: 86400,
+      week: 604800,
+      month: 2592000,
+      year: 31536000
+    }
+  }
 };
