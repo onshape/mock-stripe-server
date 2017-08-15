7e65b2ff372b8b897c357e299a36586b26001386
mbarr <mbarr@onshape.com>
2017-08-15 14:48:43 -0400

    Better command line option handling and start / stop with pid file.

--- index.js
+++ index.js
@@ -2,21 +2,50 @@
 'use strict';
 
 const fs = require('fs');
+const child_process = require('child_process');
 
-let config;
+const pidFile = '/tmp/mock-stripe-server.pid';
 
-const options = require('minimist')(process.argv.slice(2));
+const args = process.argv.slice(2);
+const options = require('minimist')(args);
+options.pidFile = pidFile;
 
-if (options.c) {
-  console.log(`Loading config file ${ options.c }...`);
-  try {
-    config = JSON.parse(fs.readFileSync(options.c));
-  } catch (error) {
-    console.log('Failed to load config file: ', error);
+if (options._.includes('start')) {
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] === 'start') {
+      args.splice(i, 1);
+      break;
+    }
   }
+  const spawn = child_process.spawn(process.argv[1], args, {
+    argv0: process.argv[0],
+    detached: true,
+    stdio: 'inherit'
+  });
+  process.exit(0);
+} else if (options._.includes('stop')) {
+  try {
+    const pid = parseInt(fs.readFileSync(pidFile).toString());
+    console.log(`Killing mock-stripe-server (pid:${ pid })...`);
+    process.kill(pid, 'SIGINT');
+  } catch (error) {
+    // no complaint
+  }
+} else {
+  let config;
+  options.pidFile = pidFile;
+
+  if (options.c) {
+    console.log(`Loading config file ${ options.c }...`);
+    try {
+      config = JSON.parse(fs.readFileSync(options.c));
+    } catch (error) {
+      console.log('Failed to load config file: ', error);
+    }
+  }
+
+  const StripeServer = require('./lib/stripeServer');
+  const stripeServer = new StripeServer(options, config);
+
+  stripeServer.boot();
 }
-
-const StripeServer = require('./lib/stripeServer');
-const stripeServer = new StripeServer(config);
-
-stripeServer.boot();
