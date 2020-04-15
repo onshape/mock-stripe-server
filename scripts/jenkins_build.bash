c095b6588884c67039b5653e5ec66e7ea5b600fd
jmorgan <jmorgan@onshape.com>
2020-04-14 22:33:19 -0400

    BEL-135067: migrate docker registry usage to AWS/ECR.

--- scripts/jenkins_build.bash
+++ scripts/jenkins_build.bash
@@ -2,6 +2,12 @@
 
 export PATH=${HOME}/build_tools/node-latest/bin:${HOME}/build_tools/yarn-latest/bin:$PATH
 
+rm -rf ./build-tools
+git clone git@github.com:onshape/build-tools.git
+pushd build-tools
+. ./buildenv.bash
+popd
+
 # test
 rm -rf ./node_modules
 yarn install
